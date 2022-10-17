91347b1e69369ed26ea8b6b6dfa2ae811e3d16c1
Jegajeeth <jegajeeth.manikandan@increscotech.com>
2022-10-17 18:59:14 +0530

    fix(Api): menuApiresponse in

---
+++ apiresponces/menuItems.js
@@ -0,0 +1,15 @@
+const menuitems = [
+  {name: 'veg rice', perprice: 40, currency: 'INR', rating: 4.5},
+  {name: 'momos', perprice: 30, currency: 'INR', rating: 4.5},
+  {name: 'shrimp fry', perprice: 80, currency: 'INR', rating: 4.3},
+  {name: 'shrimp', perprice: 80, currency: 'INR', rating: 4.2},
+  {name: 'sh', perprice: 70, currency: 'INR', rating: 4},
+  {name: 'fry', perprice: 70, currency: 'INR', rating: 4.4},
+  {name: 'rimp fry', perprice: 65, currency: 'INR', rating: 3.5},
+  {name: 'momos fry', perprice: 60, currency: 'INR', rating: 2},
+  {name: 'veg fry', perprice: 80, currency: 'INR', rating: 4.5},
+  {name: 'rice fry', perprice: 90, currency: 'INR', rating: 4.9},
+  {name: 'shrimp gravy', perprice: 180, currency: 'INR', rating: 4.1},
+];
+
+export default menuitems;
--- screens/Info.js
+++ screens/Info.js
@@ -7,26 +7,13 @@ import InfoContent from '../components/Info/InfoContent';
 import InfoImg from '../components/Info/InfoImg';
 import ListDrawerComp from '../components/ListDrawerComp';
 import store from '../store/store';
+import menuitems from '../apiresponces/menuItems';
 
 const Info = ({navigation, route}) => {
   const [showCartDraw, setShowCartDraw] = useState(false);
   const restData = route.params.restData;
   const theme = store.getState().theme;
   const cartitems = store.getState().cartitems;
-
-  const menuitems = [
-    {name: 'veg rice', perprice: 40, currency: 'INR', rating: 4.5},
-    {name: 'momos', perprice: 30, currency: 'INR', rating: 4.5},
-    {name: 'shrimp fry', perprice: 80, currency: 'INR', rating: 4.3},
-    {name: 'shrimp', perprice: 80, currency: 'INR', rating: 4.2},
-    {name: 'sh', perprice: 70, currency: 'INR', rating: 4},
-    {name: 'fry', perprice: 70, currency: 'INR', rating: 4.4},
-    {name: 'rimp fry', perprice: 65, currency: 'INR', rating: 3.5},
-    {name: 'momos fry', perprice: 60, currency: 'INR', rating: 2},
-    {name: 'veg fry', perprice: 80, currency: 'INR', rating: 4.5},
-    {name: 'rice fry', perprice: 90, currency: 'INR', rating: 4.9},
-    {name: 'shrimp gravy', perprice: 180, currency: 'INR', rating: 4.1},
-  ];
 
   const [addedItems, setAddedItems] = useState(store.getState().cartitems);
 
